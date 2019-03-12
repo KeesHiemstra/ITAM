8d96810ab40bf90fb8e5430ef4131622ecb1601e
Kees Hiemstra <chi@xs4all.nl>
2019-03-12 18:10:04 +0100

    Next step in WPF Software list

--- Software-Explorer/MainWindow.xaml.cs
+++ Software-Explorer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Software_Explorer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,8 @@ namespace Software_Explorer
   /// </summary>
   public partial class MainWindow : Window
   {
+    SoftwareList InstalledSoftware = new SoftwareList();
+
     public MainWindow()
     {
       InitializeComponent();
