5d9ad6da49968d3119a835104d23c014e4100d97
Kees Hiemstra <chi@xs4all.nl>
2019-03-26 20:01:49 +0100

    Building Inventory

---
+++ ITAMLib/Models/Win32_BaseBoard_List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ITAMLib.Models;
+
+namespace ITAMLib.Models
+{
+	public class Win32_BaseBoard_List
+	{
+		public string ComputerName { get; set; }
+		public List<Win32_BaseBoard> Items = new List<Win32_BaseBoard>();
+
+		public Win32_BaseBoard_List(string WmiClass, string members)
+		{
+			ComputerName = System.Environment.MachineName;
+			CollectWmiClass(WmiClass, members);
+		}
+
+		private async void CollectWmiClass(string wmiClass, string members)
+		{
+				Items.Clear();
+
+				try
+				{
+					foreach (ManagementObject managementObject in WmiList.GetCollection(wmiClass, members))
+					{
+						WmiRecord record = new WmiRecord(members);
+						foreach (PropertyData propertyData in managementObject.Properties)
+						{
+							record.ProcessProperty(propertyData);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"Quering the WMI results in an exception:\n{ex.Message}", "Exception", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+				}
+			}
+		}
+	}
---
+++ Inventory/Program.cs
@@ -0,0 +1,18 @@
+﻿using ITAMLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+	class Program
+	{
+		Win32_BaseBoard_List win32_Product = new Win32_BaseBoard_List("Win32_BaseBoard", "Manufacturer,SerialNumber,Version");
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
